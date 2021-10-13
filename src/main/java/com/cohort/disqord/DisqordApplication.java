8fbe83eff1b7a97ce533823034ff93a5accacc53
Sal-Nunez <88513143+Sal-Nunez@users.noreply.github.com>
2021-10-13 14:23:26 -0700

    commit

---
+++ src/main/java/com/cohort/disqord/DisqordApplication.java
@@ -0,0 +1,13 @@
+package com.cohort.disqord;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DisqordApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DisqordApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cohort/disqord/DisqordApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cohort.disqord;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DisqordApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
