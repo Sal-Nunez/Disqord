ded47849264ada81eb03f439da44b237ab058d93
Sal-Nunez <88513143+Sal-Nunez@users.noreply.github.com>
2021-10-14 14:46:32 -0700

    rest of controllers with simple crud functionality

---
+++ src/main/java/com/cohort/disqord/controllers/ServerMemberController.java
@@ -0,0 +1,117 @@
+package com.cohort.disqord.controllers;
+
+import javax.servlet.http.HttpSession;
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+
+import com.cohort.disqord.models.ServerMember;
+import com.cohort.disqord.models.User;
+import com.cohort.disqord.services.ServerMemberService;
+import com.cohort.disqord.services.UserService;
+
+@Controller
+public class ServerMemberController {
+
+    @Autowired
+    ServerMemberService serverMemberServ;
+
+    @Autowired
+    UserService userService;
+
+
+
+    @GetMapping("/serverMembers/new")
+    public String newServerMember(@ModelAttribute("serverMember") ServerMember serverMember, HttpSession session, Model model) {
+        if(session.getAttribute("uuid") == null) {
+        return "redirect:/";
+        } else {
+            Long id = (Long) session.getAttribute("uuid");
+            User user = userService.findById(id);
+            model.addAttribute("user", user);
+            return "newServerMember.jsp";        	
+        }
+    }
+
+    @PostMapping("/newServerMember")
+    public String createServerMember(
+            @Valid @ModelAttribute("serverMember") ServerMember serverMember,
+            BindingResult result, HttpSession session) {
+        if (result.hasErrors()) {
+            return "newServerMember.jsp";
+        } else {
+            serverMemberServ.updateCreate(serverMember);
+            return "redirect:/dashboard";
+        }
+    }
+
+    @GetMapping("/serverMembers/{id}/edit")
+    public String editsServerMember(
+            @ModelAttribute("serverMember") ServerMember serverMember,
+            BindingResult result, HttpSession session,
+            @PathVariable("id") Long id,
+            Model model) {
+        if(session.getAttribute("uuid") == null) {
+        return "redirect:/";
+        } else {
+            Long user_id = (Long) session.getAttribute("uuid");
+            User user = userService.findById(user_id);
+            model.addAttribute("user", user);
+            return "editServerMember.jsp";
+        }
+    }
+
+
+    @PutMapping("/serverMember/{id}/edit")
+    public String editServerMember(
+            @Valid @ModelAttribute("serverMember") ServerMember serverMember,
+            BindingResult result, HttpSession session, Model model) {
+        if (result.hasErrors()) {
+            Long user_id = (Long) session.getAttribute("uuid");
+            User user = userService.findById(user_id);
+            model.addAttribute("user", user);
+            return "editServerMember.jsp";
+        } else {
+            serverMemberServ.updateCreate(serverMember);
+            return "redirect:/dashboard";
+        }
+    }
+
+    @GetMapping("/serverMembers/{id}")
+    public String serverMember(HttpSession session,
+            @PathVariable("id") Long serverMember_id,
+            Model model) {
+        if(session.getAttribute("uuid") == null) {
+        return "redirect:/";
+        } else {
+        Long user_id = (Long) session.getAttribute("uuid");
+        User user = userService.findById(user_id);
+        ServerMember serverMember = serverMemberServ.findById(serverMember_id);
+        model.addAttribute("serverMember", serverMember);
+        model.addAttribute("user", user);
+        return "serverMember.jsp";
+        }
+    }
+    
+        @DeleteMapping("/serverMembers/{id}")
+        public String deleteServerMember(@PathVariable("id") Long serverMember_id, HttpSession session) {
+            Long id = (Long) session.getAttribute("uuid");
+            User user = userService.findById(id);
+            ServerMember serverMember = serverMemberServ.findById(serverMember_id);
+            if (serverMember.getServer().getOwner() == user || serverMember.getServerMember() == user) {
+                serverMemberServ.delete(serverMember_id);
+                return "redirect:/dashboard";
+            }
+            session.removeAttribute("uuid");
+            return "redirect:/";
+        }
+}
--- src/main/webapp/WEB-INF/Users/dashboard.jsp
+++ src/main/webapp/WEB-INF/Users/dashboard.jsp
@@ -1,7 +1,7 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8" pageEncoding="UTF-8"%>
-<!-- c:out ; c:forEach etc. --> 
+<!-- c:out ; c:forEach etc. -->
 <%@taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c"%>
-<!-- Formatting (dates) --> 
+<!-- Formatting (dates) -->
 <%@taglib uri="http://java.sun.com/jsp/jstl/fmt" prefix="fmt"%>
 <!-- form:form -->
 <%@ taglib prefix="form" uri="http://www.springframework.org/tags/form"%>
@@ -13,6 +13,7 @@
     <meta charset="UTF-8">
     <title>Dashboard</title>
     <link rel="stylesheet" href="/webjars/bootstrap/css/bootstrap.min.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <link rel="stylesheet" href="/css/style.css"> <!-- change to match your file/naming structure -->
     <script src="/webjars/jquery/jquery.min.js"></script>
