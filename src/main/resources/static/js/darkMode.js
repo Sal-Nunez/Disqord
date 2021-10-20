cfd987be5c00d9497829fef4a50ed3233c316c45
Sal-Nunez <88513143+Sal-Nunez@users.noreply.github.com>
2021-10-19 20:41:49 -0700

    formatting

--- src/main/resources/static/js/darkMode.js
+++ src/main/resources/static/js/darkMode.js
@@ -7,57 +7,52 @@ element.classList.add("lightModeText");
 
 function darkMode() {
 	text = document.querySelectorAll(".lightModeText");
-  if (checkBox.checked){
-    element.classList.add("dark-mode");
-    console.log(background.length)
-    background.forEach(e => {
-	e.classList.add("dark-mode")
-	})
-	text.forEach(e => {
-    e.classList.add("darkModeText");		
-	})
-    localStorage.setItem(
-    'theme', "dark-mode"
-  )
-  } else {
-		element.classList.remove("dark-mode");
+	if (checkBox.checked) {
+		element.classList.add("dark-mode");
 		background.forEach(e => {
-		e.classList.remove("dark-mode")
+			e.classList.add("dark-mode")
 		})
 		text.forEach(e => {
-	    e.classList.remove("darkModeText");		
+			e.classList.add("darkModeText");
 		})
 		localStorage.setItem(
-    'theme', "light-mode"
-  )
+			'theme', "dark-mode"
+		)
+	} else {
+		element.classList.remove("dark-mode");
+		background.forEach(e => {
+			e.classList.remove("dark-mode")
+		})
+		text.forEach(e => {
+			e.classList.remove("darkModeText");
+		})
+		localStorage.setItem(
+			'theme', "light-mode"
+		)
 	}
 }
 
 function darkModeCheck() {
 	if (localStorage.getItem('theme') == "dark-mode") {
 		element.classList.add("dark-mode");
-	    background.forEach(e => {
-		e.classList.add("dark-mode")
+		background.forEach(e => {
+			e.classList.add("dark-mode")
 		})
 		text.forEach(e => {
-	    e.classList.add("darkModeText");		
+			e.classList.add("darkModeText");
 		})
-    	checkBox.checked = true;
+		checkBox.checked = true;
 	}
 	else {
 		element.classList.remove("dark-mode");
 		text.forEach(e => {
-	    e.classList.remove("darkModeText");		
+			e.classList.remove("darkModeText");
 		})
 		background.forEach(e => {
-		e.classList.remove("dark-mode")
+			e.classList.remove("dark-mode")
 		})
 		checkBox.checked = false;
 	}
-}
-
-function helloWorld() {
-	console.log("Helo World!")
 }
 
 darkModeCheck();
